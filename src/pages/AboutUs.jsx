c7ae8aec07ee51049d91926e456fb0acd18937d3
Aidar-hub <146725346+Aidar-hub@users.noreply.github.com>
2024-06-17 13:12:57 +0600

    public

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,10 +1,14 @@
+import { t } from 'i18next'
 import React from 'react'
 
 export default function AboutUs() {
     return (
         <div className='container'>
-            <div className="about">
-                
+            <div className="about-us">
+                <h1 className='title-page'> {t("about")} </h1>
+                <div className="we-will-help">
+                    
+                </div>
             </div>
         </div>
     )
