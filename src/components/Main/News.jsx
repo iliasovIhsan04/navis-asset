3295f497b045824b73125f415b4c7605ba58ad3b
liasovIhsan04 <liasovihsan1@gmail.com>
2024-06-18 23:42:31 +0600

    main

--- src/components/Main/News.jsx
+++ src/components/Main/News.jsx
@@ -1,21 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { Main } from "../../api";
 
-
 const News = () => {
-const [news, setNews] = useState([]);
-useEffect(() => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
     const fetchData = async () => {
       const res = await Main.getNews();
       setNews(res);
     };
     fetchData();
   }, []);
+
+  console.log(news);
+
   return (
     <div className="container">
       <div className="news wallet-recovery">
-        <div className="news-box"></div>
-        <div className="news-box"></div>
+        <div className="news-box"> </div>
+        <div className="news-box">
+          {news?.map((newsItem) => (
+            <div key={newsItem.id}>
+              <div>
+                <h1>{newsItem.title}</h1>
+                <p>{newsItem.description}</p>
+                {newsItem.comments.map((comment, index) => (
+                  <div key={index}>
+                    <h2>{comment.author}</h2>
+                    <p>{comment.created_at}</p>
+                    <p>{comment.content}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
