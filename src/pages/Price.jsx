85a9803fef208ded5653f66fbaee1428c6c37ca2
liasovIhsan04 <liasovihsan1@gmail.com>
2024-06-23 03:50:49 +0600

    ihsan

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -1,53 +1,74 @@
-import React from 'react'
-import { t } from 'i18next'
-import check from "../assets/img/check.svg"
+import check from "../assets/img/checkImg.svg";
+import { useTranslation } from "react-i18next";
 
 export default function Price() {
-    return (
-        <div className="container">
-            <div className='wallet-recovery'>
-                <div className="price-card customer-card">
-                    <h1 className="title-page"> {t("price")} </h1>
-                    <p className='text-span'>
-                        {t("priceText1")}
-                        <br />
-                        {t("priceText2")}
-                        <br />
-                        {t("priceText3")}
-                        <br />
-                        {t("priceText4")}
-                    </p>
-                </div>
-                <div className="price-card customer-card white">
-                    <h2 className='subtitle2 subtitle1'> {t("walletRecovery")} </h2>
-                    <h1 className="title-page l white"> 25% <span className="oval1 white"> {t("fromTheWallet")} </span> </h1>
-                    <div className="checks d-grid">
-                        <div className="check-card d-flex align-items-center g-10">
-                            <img src={check} alt="" />
-                            <h3 className='title-basic'> {t("checkTitle1")} </h3>
-                        </div>
-                        <div className="check-card d-flex align-items-center g-10">
-                            <img src={check} alt="" />
-                            <h3 className='title-basic'> {t("checkTitle2")} </h3>
-                        </div>
-                        <div className="check-card d-flex align-items-center g-10">
-                            <img src={check} alt="" />
-                            <h3 className='title-basic'> {t("checkTitle3")} </h3>
-                        </div>
-                        <div className="check-card d-flex align-items-center g-10">
-                            <img src={check} alt="" />
-                            <h3 className='title-basic'> {t("checkTitle4")} </h3>
-                        </div>
-                        <div className="check-card d-flex align-items-center g-10">
-                            <img src={check} alt="" />
-                            <h3 className='title-basic'> {t("checkTitle5")} </h3>
-                        </div>
-                    </div>
-                    <button className='btn-white d-flex align-items-center justify-content-center'>
-                        {t("startRecovery")}
-                    </button>
-                </div>
-            </div>
+  const { t } = useTranslation();
+  const data = [
+    {
+      id: 1,
+      img: check,
+      text: t("checkTitle1"),
+    },
+    {
+      id: 2,
+      img: check,
+      text: t("checkTitle2"),
+    },
+    {
+      id: 3,
+      img: check,
+      text: t("checkTitle3"),
+    },
+    {
+      id: 4,
+      img: check,
+      text: t("checkTitle4"),
+    },
+    {
+      id: 5,
+      img: check,
+      text: t("checkTitle5"),
+    },
+  ];
+  return (
+    <div className="container">
+      <div className="wallet-recovery">
+        <div className="price-card customer-card">
+          <h2 className="big-text price-title"> {t("price")} </h2>
+          <p className="text-span">
+            {t("priceText1")}
+            <br />
+            {t("priceText2")}
+            <br />
+            {t("priceText3")}
+            <br />
+            {t("priceText4")}
+          </p>
         </div>
-    )
+        <div className="price-card customer-card">
+          <h2 className="subtitle2 subtitle1"> {t("walletRecovery")} </h2>
+          <h2 className="big-text">
+            25%{" "}
+            <span className="price-wallet-text"> {t("fromTheWallet")} </span>{" "}
+          </h2>
+          <div className="checks d-grid">
+            {data?.map((el) => (
+              <div
+                key={el.id}
+                className="check-card d-flex align-items-center g-10"
+              >
+                <div className="check-card-img d-flex d-flex align-items-center justify-content-center">
+                  <img src={el.img} alt="" />
+                </div>
+                <h3 className="title-basic">{el.text} </h3>
+              </div>
+            ))}
+          </div>
+          <button className="btn-white d-flex align-items-center justify-content-center">
+            {t("startRecovery")}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
